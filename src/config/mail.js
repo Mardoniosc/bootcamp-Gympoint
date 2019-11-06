a9e265c1d62cd671b28b66b4c1a6e768845f7233
Mardoniosc <Mardonio@live.com>
2019-11-05 22:15:09 -0300

    Configurado as variaveis ambiente da aplicação

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '71f622c934080a',
-    pass: '4083adf38fe417',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe Gimpoint <noreply@gympoint.com>',
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://83b114987c04496383a5e45e73e54fd0@sentry.io/1809186',
+  dsn: process.env.SENTRY_DSN,
 };
